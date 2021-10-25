461762fc0c2c0275056f027e6b9d66f4f1cfa753
David Cruciani <da.cruciani@laposte.net>
2021-10-25 15:26:30 +0200

    client for serveur

--- Others/AutoGenYara/Serveur/bin/OnWindows/VarClient.py
+++ Others/AutoGenYara/Serveur/bin/OnWindows/VarClient.py
@@ -1,11 +1,11 @@
 #Path to folder who will contains software installer and task file (ShareFolder)
-pathToInstaller = "\\\VBOXSVR\\ShareFolder_VM\\Installer"
+pathToInstaller = "\\\VBOXSVR\\PartageVM\\Installer"
 
 #Path to folder use for the excratction of softawre exe (ShareFolder)
-pathToExeExtract = "\\\VBOXSVR\\ShareFolder_VM\\exe_extract"
+pathToExeExtract = "\\\VBOXSVR\\PartageVM\\exe_extract"
 
 #Path to Uninstall.exe (Disk)
-pathToUninstaller = "\\\VBOXSVR\\ShareFolder_VM\\UninstallX64.exe"
+pathToUninstaller = "\\\VBOXSVR\\PartageVM\\UninstallX64.exe"
 
 pathToSDelete = "C:/Users/Administrateur/Downloads/sdelete64.exe"
 
@@ -14,8 +14,11 @@ pathToSDelete = "C:/Users/Administrateur/Downloads/sdelete64.exe"
 pathToAsa = "C:/Users/Administrateur/Downloads/ASA_win_2.3.268/Asa.exe"
 
 #Path to the folder who will contains AsaReport (ShareFolder)
-pathToAsaReport = "\\\VBOXSVR\\ShareFolder_VM\\AsaReport\\"
+pathToAsaReport = "\\\VBOXSVR\\PartageVM\\AsaReport\\"
 #pathToAsaReport = ""
 
 #Path to result of systeminfo
-pathToSysInfo = "\\\VBOXSVR\\ShareFolder_VM\\sysinfo.txt"
+pathToSysInfo = "\\\VBOXSVR\\PartageVM\\sysinfo.txt"
+
+#Path to log record during execution of client
+pathToLogclient = "\\\VBOXSVR\\PartageVM\\logClient.txt"
